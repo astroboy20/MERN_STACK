0450dc93382a70c2770837f5fa1fd5d7faa9e722
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-09-21 08:44:06 +0100

    feat: set up logic for get & create workout

---
+++ frontend/src/components/WorkoutDetails.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load(kg): {workout.load}</strong>
+      </p>
+      <p>
+        <strong>Reps: {workout.load}</strong>
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
